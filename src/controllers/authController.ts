22a546db2133ebe8b937692f9d9ff8ab58b7ae1f
AnyuEscobar <mdlaescobarr@gmail.com>
2025-11-18 21:24:49 -0300

    add: creacion del authcontroller de register

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express"
 import bcrypt from "bcryptjs"
 import User from "../model/UserModel"
+import jwt from "jsonwebtoken"
 
 const SECRET_KEY = 1234
 
@@ -25,9 +26,31 @@ class AuthController {
   }
   static login = async (req: Request, res: Response): Promise<void | Response> => {
     try {
+      const { email, password } = req.body
 
-    } catch (error) {
+      if (!email || !password) {
+        return res.status(400).json({ success: false, error: "Datos inválidos" })
+      }
 
+      const user = await User.findOne({ email })
+
+      if (!user) {
+        return res.status(401).json({ success: false, error: "No autorizado" })
+      }
+      const isValid = await bcrypt.compare(password, user.password)
+
+      if (!isValid) {
+        return res.status(401).json({ success: false, error: "No autorizado" })
+      }
+
+      const token = jwt.sign({ id: user._id }, SECRET_KEY, { expiresIn: "1h" })
+      res.status(200).json({ success: true, token })
+
+    } catch (e) {
+      const error = e as Error
+      res.status(500).json({ success: false, error: "Sucedió un error" })
     }
   }
 }
+
+export default AuthController
